e688a80c635a631544ddfc70c4082b5a899dad9f
MarkoMark98 <52664407+MarkoMark98@users.noreply.github.com>
2019-12-16 18:36:25 +0100

    component informazioni bando

--- src/components/NoticeInformation/NoticeInformation.jsx
+++ src/components/NoticeInformation/NoticeInformation.jsx
@@ -1,8 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import {Card} from "../Card/Card";
 
 export class NoticeInformation extends Component {
+
     render() {
-        
+        const {notice} = this.props;
+        return (
+            <div>
+                <Card
+                    title = {notice.name}
+                    content = {
+                        <div>
+                            {notice.articoli.map((current) =>{
+                                return (<Fragment>
+                                    <h4 style={{"margin-left": "10px"}}>{current.title}</h4>
+                                        <p style={{"margin-left": "20px"}}>{current.body}</p>
+                                </Fragment>)
+                            })}
+                        </div>
+                    }
+                />
+            </div>
+        )
     }
 }
 
--- src/views/DetailsNotice.jsx
+++ src/views/DetailsNotice.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
-import Upload from "../components/UploadDocuments/Upload";
+import NoticeInformation from "../components/NoticeInformation/NoticeInformation";
 
 class DetailsNotice extends Component {
     render() {
-        return(<Upload/>)
+        return(<NoticeInformation notice={notice}/>)
     }
 }
 export default DetailsNotice;
+
+const notice = {
+    name: "Bando numero 1",
+    articoli: [
+        {
+            title: "Articolo 1",
+            body: "corpo dell'articolo"
+        },
+        {
+            title: "Articolo 2",
+            body: "corpo dell'articolo"
+        },
+        {
+            title: "Articolo 3",
+            body: "corpo dell'articolo"
+        }
+    ]
+}
