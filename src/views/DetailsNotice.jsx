c985272533baa150d5cfb78d2e6eca9fa3653834
MarkoMark98 <52664407+MarkoMark98@users.noreply.github.com>
2019-12-17 19:45:39 +0100

    merge dei conflitti su details notice

--- src/views/DetailsNotice.jsx
+++ src/views/DetailsNotice.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import NoticeInformation from "../components/NoticeInformation/NoticeInformation";
 import Upload from "../components/UploadDocuments/Upload";
 
 class DetailsNotice extends Component {
     render() {
-        return(<NoticeInformation notice={notice}/>)
-        return <Upload />;
+        return(
+            <Fragment>
+                <NoticeInformation notice={notice}/>
+                <Upload />
+            </Fragment>
+        );
     }
 }
 
