6a54a57190144583afc4e58cb44a0d0c3074023b
MarkoMark98 <52664407+MarkoMark98@users.noreply.github.com>
2019-12-18 02:50:55 +0100

    component noticeInformation [COMPLETATO]

--- src/components/NoticeInformation/NoticeInformation.jsx
+++ src/components/NoticeInformation/NoticeInformation.jsx
@@ -2,22 +2,46 @@ import React, { Component, Fragment } from "react";
 import {Card} from "../Card/Card";
 
 export class NoticeInformation extends Component {
-
     render() {
+        const today  = new Date()
+        this.state = {date: today.getFullYear()}
         const {notice} = this.props;
         return (
             <div>
                 <Card
                     title = {notice.name}
                     content = {
-                        <div>
-                            {notice.articoli.map((current) =>{
-                                return (<Fragment>
-                                    <h4 style={{"margin-left": "10px"}}>{current.title}</h4>
-                                        <p style={{"margin-left": "20px"}}>{current.body}</p>
-                                </Fragment>)
+                        <Fragment>
+                            <p className="pull-right">
+                                Rep. n: <i>
+                                            <u>{notice.n_rep}</u>/{this.state.date}
+                                        </i>
+                            </p>
+                            <div className="margin-top">
+                                <b className="text-center center-block">{notice.description}</b>
+                                <b className="text-center center-block margin-top">IL DIRETTORE</b>
+                                {notice.notifications.map((current) =>{
+                                    return (
+                                        <Fragment>
+                                            <b style={{"margin": "10px"}}>{current.title}</b>
+                                            <p style={{"display": "inline"}}>{current.body}</p>
+                                            <br></br>
+                                        </Fragment>
+                                    )
+                                })}
+                            </div>
+                            {notice.articles.map((current) =>{
+                                return (
+                                    <Fragment>
+                                        <u><b><h4 style={{"paddingLeft": "10px", "paddingRight": "10px"}}>{current.title}</h4></b></u>
+                                        <i style={{"paddingLeft": "20px", "paddingRight": "20px"}}>{current.content}</i>
+                                    </Fragment>
+                                )
                             })}
-                        </div>
+                            <p className="margin-top">{notice.retification}</p>
+                                <u><b><h4 className="margin-top" style={{"paddingLeft": "10px", "paddingRight": "10px"}}>TRATTAMENTO DEI DATI PERSONALI</h4></b></u>
+                            <p style={{"paddingLeft": "20px", "paddingRight": "20px"}}>{notice.personal_data_treatment}</p>
+                        </Fragment>
                     }
                 />
             </div>
--- src/views/DetailsNotice.jsx
+++ src/views/DetailsNotice.jsx
@@ -1,9 +1,65 @@
 import React, { Component, Fragment } from "react";
 import NoticeInformation from "../components/NoticeInformation/NoticeInformation";
 import Upload from "../components/UploadDocuments/Upload";
+import notice from "../static"
 
 class DetailsNotice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          noticeJSON: {}
+        };
+    }
+
+    componentDidMount() {
+        //Fetch file notice.json from Back-end github project
+        fetch("https://raw.githubusercontent.com/fabiolademarco/MyTutorBack-End/master/static/notice.json")
+        //Transform response text in json object
+        .then(blob => blob.json())
+        //set the json object in the state
+        .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    noticeJSON: result
+                });
+            },(error) => {
+                this.setState({
+                    isLoaded: false,
+                    error
+                });
+            }
+        )
+    }
+
     render() {
+        const{noticeJSON, isLoaded} = this.state;
+
+        //check if the fetch of the JSON file it was successfull
+        if(isLoaded) {
+            //fill the notice object with the field of the JSON object
+            notice["director"] = noticeJSON.director
+            notice["decrees"] = noticeJSON.decrees
+            notice["ratification"] =  noticeJSON.ratification
+            notice["personal_data_treatment"] = noticeJSON.personal_data_treatment
+
+            //recovery of the articles form JSON object
+            var articlesJSON = this.state.noticeJSON;
+            for (var obj in articlesJSON) {
+                //check if the entry of JSON object is an article
+                if(obj.startsWith("art")){
+                    //check if the article is not the 13th
+                    if(!obj.endsWith("13")) {
+                        //add the attribute content at the article
+                        articlesJSON[obj]["content"] = "Contenuto dell'articolo"
+                    }
+                    //add the articles in the notice
+                    notice.articles.push(articlesJSON[obj])
+                }
+            }
+        }
+
         return(
             <Fragment>
                 <NoticeInformation notice={notice}/>
@@ -13,21 +69,4 @@ class DetailsNotice extends Component {
     }
 }
 
-const notice = {
-    name: "Bando numero 1",
-    articoli: [
-        {
-            title: "Articolo 1",
-            body: "corpo dell'articolo"
-        },
-        {
-            title: "Articolo 2",
-            body: "corpo dell'articolo"
-        },
-        {
-            title: "Articolo 3",
-            body: "corpo dell'articolo"
-        }
-    ]
-}
 export default DetailsNotice;
