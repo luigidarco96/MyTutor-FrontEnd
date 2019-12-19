2fed1029d092a625335e75e0e79aea1fa2f361ac
Pepp1 <giuseppe.facenda@live.it>
2019-12-19 16:36:32 +0100

    Modifiche Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Row,
+  Col
+} from "react-bootstrap";
 import "../assets/css/page-login.css";
+import Card from "../components/Card/Card";
+import Button from "components/CustomButton/CustomButton";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
@@ -15,29 +23,44 @@ export default function Login(props) {
   }
 
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
+    <Row className="Login_Card">
+      <Col md="4"></Col>
+      <Col md="4">
+        <Card
+          content={
+            <div className="Login">
+              Login
+              <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                  <ControlLabel>Email</ControlLabel>
+                  <FormControl
+                    autoFocus
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                  />
+                </FormGroup>
+                <Button
+                  block
+                  bsSize="large"
+                  disabled={!validateForm()}
+                  type="submit"
+                >
+                  Invio
+                </Button>
+              </form>
+            </div>
+          }
+        />
+      </Col>
+    </Row>
   );
 }
