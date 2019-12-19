b2b2f2c7b5cabf523cbf6dfeb4c0f2a9fabe9319
Pepp1 <giuseppe.facenda@live.it>
2019-12-19 16:36:12 +0100

    modifiche su page-login.css l'upload dei file funziona!

--- src/components/UploadDocuments/Upload.jsx
+++ src/components/UploadDocuments/Upload.jsx
@@ -1,47 +1,60 @@
 import React, { Component, useCallback, useState } from "react";
-import Dropzone from "../Dropzone/Dropzone";
+import { useDropzone } from "react-dropzone";
+import Card from "../Card/Card";
+import Button from "components/CustomButton/CustomButton";
 
-class Upload extends Component {
-  onDrop = acceptedFiles => {
+const Upload = props => {
+  const maxSize = 1048576;
+
+  const onDrop = useCallback(acceptedFiles => {
     console.log(acceptedFiles);
-  };
+  }, []);
 
-  render() {
-    const maxSize = 1048576;
-    return (
-      <div className="text-center mt-5">
-        <Dropzone
-          onDrop={this.onDrop}
-          accept="image/png"
-          minSize={0}
-          maxSize={maxSize}
-          multiple
-        >
-          {({
-            getRootProps,
-            getInputProps,
-            isDragActive,
-            isDragReject,
-            rejectedFiles
-          }) => {
-            const isFileTooLarge =
-              rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
-            return (
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                {!isDragActive && "Click here or drop a file to upload!"}
-                {isDragActive && !isDragReject && "Drop it like it's hot!"}
-                {isDragReject && "File type not accepted, sorry!"}
-                {isFileTooLarge && (
-                  <div className="text-danger mt-2">File is too large.</div>
-                )}
-              </div>
-            );
-          }}
-        </Dropzone>
-      </div>
-    );
-  }
-}
+  const {
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    isDragReject,
+    acceptedFiles,
+    rejectedFiles
+  } = useDropzone({
+    onDrop,
+    accept: "application/pdf",
+    minSize: 0,
+    maxSize
+  });
+
+  const isFileTooLarge =
+    rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+
+  return (
+    <Card
+      title="Craica Documenti Candidatura"
+      content={
+        <div className="text-center">
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {!isDragActive && "Click here or drop a file to upload!"}
+            {isDragActive && !isDragReject && "Drop it like it's hot!"}
+            {isDragReject && "File type not accepted, sorry!"}
+            {isFileTooLarge && (
+              <div className="text-danger mt-2">File is too large.</div>
+            )}
+          </div>
+
+          <ul className="list-group mt-2">
+            {acceptedFiles.length > 0 &&
+              acceptedFiles.map(acceptedFile => (
+                <li className="list-group-item list-group-item-success">
+                  {acceptedFile.name}
+                </li>
+              ))}
+          </ul>
+          <Button bsStyle="primary">Invia Candidatura</Button>
+        </div>
+      }
+    />
+  );
+};
 
 export default Upload;
