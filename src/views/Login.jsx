8dee98fde519d33026aa38b01aee70833cb6c24d
Pepp1 <giuseppe.facenda@live.it>
2019-12-19 14:39:49 +0100

    Aggiunta pagina di login e modificato file index.js

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,14 +1,43 @@
-import React, { Component } from "react";
-import { Card } from "../components/Card/Card";
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "../assets/css/page-login.css";
 
-export class Login extends Component {
-  render() {
-    return (
-      <div>
-        <Card Component={"ciao"} />
-      </div>
-    );
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
   }
-}
 
-export default Login;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
