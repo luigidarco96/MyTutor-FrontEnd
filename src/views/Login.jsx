078738cdd0bb234d3129e1a5e123de971bc203cd
Pepp1 <giuseppe.facenda@live.it>
2019-12-19 12:38:21 +0100

    aggiunta pagina di Login

---
+++ src/views/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Card } from "../components/Card/Card";
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Card Component={"ciao"} />
+      </div>
+    );
+  }
+}
+
+export default Login;
