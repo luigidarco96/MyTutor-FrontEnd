cea2fa1fbd2bd2663ae818ea63846c77d7b2626a
Lorenzo <lorenzocarpentieri34@gmail.com>
2019-12-21 13:11:03 +0100

    Modificate routes

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ import "./assets/css/pe-icon-7-stroke.css";
 import Notices from 'views/Notices';
 import AdminLayout from "layouts/Admin.jsx";
 import Login from "./views/Login";
-import SignUp from "./views/Login";
+import SignUp from "./views/SignUp";
 
 ReactDOM.render(
   <BrowserRouter>
