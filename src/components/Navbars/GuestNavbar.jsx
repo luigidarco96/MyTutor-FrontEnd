90b55f2d813c39d786e928bb66807522b92f5574
MarkoMark98 <52664407+MarkoMark98@users.noreply.github.com>
2019-12-22 21:51:41 +0100

    sign up [COMPLETE]

---
+++ src/components/Navbars/GuestNavbar.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap";
+
+export class GuestNavbar extends Component {
+    render() {
+        const positionImgStyle={
+            fontWeight: "400",
+            margin: "5px 0px",
+            padding: "2px 15px",
+            fontSize: "50px",
+            textDecoration:"none",
+            color:"white",
+          }
+          const spanStyle={
+            padding:"0px 0px" ,
+            marginBottom: "12px",
+          }
+        return (
+            <Navbar fluid style={{ backgroundColor: "#274F77"}}>
+                <Navbar.Header>
+
+                    <div style={spanStyle}>
+                        <a
+                            href="http://localhost:3000/home"
+                            style={positionImgStyle}
+                        >
+                            <img style={{ height: "50px" }} src="/assets/images/logo_progetto_tutorato_light.png" alt="logo_image" />
+                            <span style={{ fontSize: "15px", marginTop: "30px", marginLeft: "4px", position: "absolute" }}> MYTUTOR</span>
+                        </a>
+                    </div>
+                    <Navbar.Toggle onClick={this.mobileSidebarToggle} />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default GuestNavbar;
--- src/components/SignUp/SignUpProfessor.jsx
+++ src/components/SignUp/SignUpProfessor.jsx
@@ -12,17 +12,19 @@ export class SignUpProfessor extends Component {
         return (
             <Form>
                     <FormGroup>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Nome</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="text"
                                 placeholder="Scrivi il tuo nome.."
                             />
                         </Col>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Cognome</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="text"
                                 placeholder="Scrivi il tuo cognome.."
@@ -30,9 +32,10 @@ export class SignUpProfessor extends Component {
                         </Col>
                     </FormGroup>
                     <FormGroup>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Data Di Nascita</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="text"
                                 placeholder="La tua data di nascita.."
@@ -40,17 +43,19 @@ export class SignUpProfessor extends Component {
                         </Col>
                     </FormGroup>
                     <FormGroup>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Email</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="text"
                                 placeholder="Scrivi la tua email.."
                             />
                         </Col>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Conferma Email</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="text"
                                 placeholder="Conferma la tua email.."
@@ -58,17 +63,19 @@ export class SignUpProfessor extends Component {
                         </Col>
                     </FormGroup>
                     <FormGroup>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Password</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="password"
                                 placeholder="Scrivi la tua password.."
                             />
                         </Col>
-                        <Col md={6} lg={6} sm={6}>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
                             <ControlLabel style={{ 'margin-top': '3px' }}>Conferma password</ControlLabel>
                             <FormControl
+                                style={{width:'400px'}}
                                 required
                                 type="password"
                                 placeholder="Conferma la tua password.."
@@ -76,9 +83,10 @@ export class SignUpProfessor extends Component {
                         </Col>
                     </FormGroup>
                     <FormGroup>
-                        <Col md={6} lg={6} sm={6}>
-                        <ControlLabel style={{ visibility: 'hidden' }}>Conferma password</ControlLabel>
-                        <FormControl style={{ visibility: 'hidden' }}/>
+                        <Col md={6} lg={6} sm={6} style={{padding:'6px'}}>
+                        <ControlLabel style={{ visibility: 'hidden','margin-top': '3px' }}>Conferma password</ControlLabel>
+                        <FormControl
+                        style={{width:'400px'}}    style={{ visibility: 'hidden' }}/>
                         </Col>
                     </FormGroup>
                 <Button
