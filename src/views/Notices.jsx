8141b26317f29c120cf11dc6010fc10d249982c3
allegrettif <f.allegretti1@gmail.com>
2020-01-02 17:32:46 +0100

    Modifiche: Notices.jsx e aggiunta dipendenza per fuse.js

--- src/views/Notices.jsx
+++ src/views/Notices.jsx
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import Axios from 'axios';
+import Fuse from 'fuse.js';
 
 import Card from 'components/Card/Card.jsx';
 import UsersTabs from '../components/UsersTabs/UsersTabs';
@@ -27,23 +28,58 @@ class Notices extends Component {
     };
   }
 
+  handleSearch(e) {
+    const { notices } = this.state;
+
+    var options = {
+      shouldSort: true,
+      threshold: 0,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: ['protocol', 'description']
+    };
+    var fuse = new Fuse(notices, options); // "list" is the item array
+    var result = fuse.search(e.target.value);
+
+    console.log(e.target.value);
+    console.log(result);
+  }
+
   handleDifferentUsers(pathname, notices) {
     let user = JSON.parse(localStorage.getItem('user'));
     if (user !== null) {
       return (
-        <UsersTabs
-          pathname={pathname}
-          notices={notices}
-          tabs={UserLists[user.role]}
-        />
+        <div className='container-fluid'>
+          <input
+            type='text'
+            name='search'
+            placeholder='Cerca bando...'
+            onChange={e => this.handleSearch(e)}
+          />
+          <UsersTabs
+            pathname={pathname}
+            notices={notices}
+            tabs={UserLists[user.role]}
+          />
+        </div>
       );
     } else {
       return (
-        <UsersTabs
-          pathname={pathname}
-          notices={notices}
-          tabs={UserLists['Student']}
-        />
+        <div className='container-fluid'>
+          <input
+            type='text'
+            name='search'
+            placeholder='Cerca bando...'
+            onChange={e => this.handleSearch(e)}
+          />
+          <UsersTabs
+            pathname={pathname}
+            notices={notices}
+            tabs={UserLists['Student']}
+          />
+        </div>
       );
     }
   }
