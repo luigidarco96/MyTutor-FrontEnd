0b8dacb1d2fa26f5c58ef5b5a56423e3f67d86ba
Marco Marchionno <marko_mark98@live.it>
2020-01-06 02:51:39 +0100

    DraftNotice [COMPLETA]

--- src/views/DraftNotice.jsx
+++ src/views/DraftNotice.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Grid, Row, Col } from 'react-bootstrap';
 import CreateNotice from './CreateNotice'
+import CreateApplication from './CreateApplication'
+import Card from 'components/Card/Card.jsx';
 
 export default class draftNotice extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          selectedTab: 'bando'
+            selectedTab: 'bando'
         };
-      }
-      
+    }
+
     handleTabSelect(e) {
 
         this.setState({
@@ -18,18 +20,31 @@ export default class draftNotice extends Component {
     }
 
     render() {
-        const {selectedTab} = this.state;
+        const { selectedTab } = this.state;
         return (
-        <Tabs
-            id='users-tabs'
-            defaultActiveKey={selectedTab}
-            onSelect={e => this.handleTabSelect(e)}
-            animation={false}>
-            <Tab eventKey={'bando'} title={'bando'} key={1}>
-                <CreateNotice {...this.props}></CreateNotice>
-            </Tab>
-        </Tabs>
-            
+            <div className='content' style={{height:'40vw', overflowY:'scroll'}}>
+                <Card
+                    title={selectedTab == 'bando' ? 'Crea Bando' : 'Crea Domanda'}
+                    ctTableFullWidth
+                    ctTableResponsive
+                    content={
+                        <div className='container-fluid'> 
+                            <Tabs
+                                id='users-tabs'
+                                defaultActiveKey={selectedTab}
+                                onSelect={e => this.handleTabSelect(e)}
+                                animation={false}>
+                                <Tab eventKey={'bando'} title={'bando'} key={1}>
+                                    <CreateNotice {...this.props}></CreateNotice>
+                                </Tab>
+                                <Tab eventKey={'domanda'} title={'domanda'} key={2}>
+                                    <CreateApplication></CreateApplication>
+                                </Tab>
+                            </Tabs>
+                        </div>
+                    }
+                />
+            </div>
         )
     }
 }
