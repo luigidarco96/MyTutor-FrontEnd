ab4e3ef2939e3fca780aaae7f68211768fc93c6d
Lorenzo <lorenzocarpentieri34@gmail.com>
2020-01-06 20:13:50 +0100

    Inserisci email verificata per il professore completato

--- src/components/Users/ProfessorList.jsx
+++ src/components/Users/ProfessorList.jsx
@@ -1,58 +1,86 @@
-import React,{Component} from 'react';
-import {Grid, Row, Table, Col,Modal} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Grid, Row, Table, Col, Modal } from 'react-bootstrap';
 import Button from '../CustomButton/CustomButton';
 import { login, logout } from '../../utils/auth';
 import axios from 'axios';
 
-export class ProfessorList extends Component{
-    state={
-        header:[],
-        professors:[],
-        isLoading: true,
-        show: false,
-        showInsertEmail:false,
-        selectedProfessor:'',
-    }
+export class ProfessorList extends Component {
+  state = {
+    header: [],
+    professors: [],
+    isLoading: true,
+    show: false,
+    showInsertEmail: false,
+    selectedProfessor: '',
+  }
 
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount(){
-        
+  componentDidMount() {
+    //Set the header of a professor table
+    this.setState({
+      header: ['E-mail', 'Nome', 'Cognome']
+    });
+    //data use to server call 
+    let data = {
+      param: {
+        role: 'Professor',
+      }
+    }
+    //headers to authenticate user
+    let headers = {
+      'Authorization': localStorage.getItem('token'),
+    }
+    //Take all professor.
+    axios.post('http://localhost:3001/api/users/search', data, {
+      headers: headers,
+    })
+      .then(blob => {
         this.setState({
-            header:['E-mail','Nome','Cognome']
-        });
-        let data ={
-          param:{
-            role:'Professor',
-          }
-        }
-
-        let headers ={
-          'Authorization':localStorage.getItem('token'),
-        }
-        axios.post('http://localhost:3001/api/users/search',data,{
-          headers:headers,
+          professors: blob.data.list,
         })
-        .then(blob=>{
-          this.setState({
-            professors: blob.data.list,
-          })
-        });
+      });
 
-
-        
+  }
+  render() {
+    //Insert email professor verfied.
+    const setEmailVerified = (emailVerified)=>{
+      const emailProfessorExp = /^[a-z]*(\.[a-z]*)?\@unisa\.it$/;
+     
+      if (!emailProfessorExp.test(emailVerified)) {
+        if (document.getElementById('2') != null) {
+          let el = document.getElementById('2');
+          el.remove();
+        }
+        var para = document.createElement("span");
+        var node = document.createTextNode("Email non valida");
+        para.appendChild(node);
+        para.style.cssText = 'color:red; margin-top:3px;';
+        para.id = '2'
+        document.getElementById('errorEmail').appendChild(para);
+      }
+      const headers = {
+        'Authorization': localStorage.getItem('token'),
+      }
+      axios
+      .post('http://localhost:3001/api/auth/verified',{email:emailVerified},{headers:headers})
+      .then(blob=>{
+        handleCloseEmail();
+        console.log(blob.data);
+      })
     }
-    render(){
-      const deleteProfessor = ()=>{
-        //Search the professor to delete.
-       
-        const id = this.state.selectedProfessor.email; 
-        let url ='http://localhost:3001/api/users/'+id;
-        axios
-        .get(url,{
-          headers:{
+    
+    //Delete professor selected.
+    const deleteProfessor = () => {
+      //Search the professor to delete.
+
+      const id = this.state.selectedProfessor.email;
+      let url = 'http://localhost:3001/api/users/' + id;
+      axios
+        .get(url, {
+          headers: {
             'Authorization': localStorage.getItem('token'),
           },
         })
@@ -60,134 +88,143 @@ export class ProfessorList extends Component{
           this.setState({
             selectedProfessor: blob.data.user,
           })
+          //headers to authenticate user
+          const headers = {
+            'Authorization': localStorage.getItem('token'),
+          }
+
+          //Delete the user selected.
+          axios.
+            delete('http://localhost:3001/api/users/' + this.state.selectedProfessor.email, {
+              headers: headers,
+            })
+            .then(blob => {
+              this.setState({
+                professors: professors.filter((el) => el.email != this.state.selectedProfessor.email),
+              })
+
+            });
+
         });
+      handleClose();
 
-        const headers={
-          'Authorization': localStorage.getItem('token'),
-        }
-        //Delete the user selected.
-        axios.
-        delete('http://localhost:3001/api/users/'+this.state.selectedProfessor.email,{
-          headers: headers,
-        },)
-        .then(blob=>{
-          this.setState({
-            professors:professors.filter((el)=>el.email!=this.state.selectedProfessor.email),
-          })
-
-        });
-        handleClose();
-        
-      }
+    }
 
 
-      const {professors} = this.state;
+    const { professors } = this.state;
 
-      const handleClose = () => this.setState({
-        show:false,
-      });
+    const handleClose = () => this.setState({
+      show: false,
+    });
 
-      const handleShow = (professor) => this.setState({
-        show:true,
-        selectedProfessor:professor,
-      });
-      const handleCloseEmail = () => this.setState({
-        showInsertEmail:false,
-      });
+    const handleShow = (professor) => this.setState({
+      show: true,
+      selectedProfessor: professor,
+    });
+    const handleCloseEmail = () => this.setState({
+      showInsertEmail: false,
+    });
 
-      const handleShowEmail = () => this.setState({
-        showInsertEmail:true,
-      });
-      const inputTextStyle={
-        height: '18px',
-        width: '100%',
-        borderRadius: '10px',
-        border: '1px solid #274F77',
-        paddingLeft: '5px',
-      }
+    const handleShowEmail = () => this.setState({
+      showInsertEmail: true,
+    });
+    const inputTextStyle = {
+      height: '18px',
+      width: '100%',
+      borderRadius: '10px',
+      border: '1px solid #274F77',
+      paddingLeft: '5px',
+    }
 
-     
-      const {
-            header,
-        }=this.state;
-        
-      const styleIconTrash={
-        fontSize:'20px',
-        color:'#274F77',
-      }
-        return (
-            <div className='content'>
-              <Grid fluid>
-              <div>   
-                  <input className='inputSearchBar' type='text' placeholder='Cerca...'/>
-                  <span><i className='pe-7s-search iconSearchBar'></i></span>
-                  <span style={{position:"absolute",right:"40px"}}>
-                    <Button onClick={handleShowEmail} style={{border:'1px solid #274F77'}} className='buttonHover'  bsStyle="primary"  bsSize="xs">
-                      Inserisci professore
+
+    const {
+      header,
+    } = this.state;
+
+    const styleIconTrash = {
+      fontSize: '20px',
+      color: '#274F77',
+    }
+    return (
+      <div className='content'>
+        <Grid fluid>
+          <div>
+            <input className='inputSearchBar' type='text' placeholder='Cerca...' />
+            <span><i className='pe-7s-search iconSearchBar'></i></span>
+            <span style={{ position: "absolute", right: "40px" }}>
+              <Button onClick={handleShowEmail} style={{ border: '1px solid #274F77' }} className='buttonHover' bsStyle="primary" bsSize="xs">
+                Inserisci professore
                     </Button>
-                  </span>
+            </span>
 
-                </div> 
+          </div>
 
-                <Row>
-                  <Col md={12}>
-                        <Table hover>
-                          <thead>
-                            <tr>
-                              {header.map((prop, key) => {
-                                return <th key={key}>{prop}</th>;
-                              })}
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {professors &&
-                              professors.map(element => {
-                                return (
-                                  <tr
-                                    key={element.serialNumber}
-                               
-                                  >
-                                    <td>{element.email}</td>
-                                    <td>{element.name}</td>
-                                    <td>{element.surname}</td>                                    
-                                    <td onClick={()=>{handleShow(element)}}><i className="pe-7s-trash trashIcon" style={styleIconTrash}></i></td>
-                                  </tr>
-                                );
-                              })}
-                          </tbody>
-                        </Table>
-                  </Col>
-                </Row>
-                
-                <Modal style={{borderRadius:'6px',overflow:'hidden',marginTop:'15%',left:'45%',position:'absolute',height:'200px',width:'350px'}} show={this.state.show} onHide={handleClose} animation={false}>
-                    <Modal.Header style={{width:'350px'}} closeButton>
-                      <Modal.Title style={{color:'#274F77'}}>Elimina professore</Modal.Title>
-                    </Modal.Header>
-        
-                    <Modal.Body style={{width:'350px',padding:'7px'}}>Confermare l'eliminazione?</Modal.Body>
-                      <Modal.Footer style={{width:'350px'}}>
-                        <Button className='buttonHover button' variant="secondary" onClick={handleClose}>Annulla</Button>
-                        <Button className='buttonHover button' variant="primary" onClick={deleteProfessor}>Elimina</Button>
-                    </Modal.Footer>
-                </Modal>              
-                <Modal style={{borderRadius:'6px',overflow:'hidden',marginTop:'15%',left:'45%',position:'absolute',height:'200px',width:'350px'}} show={this.state.showInsertEmail} onHide={handleCloseEmail} animation={false}>
-                    <Modal.Header style={{width:'350px'}} closeButton>
-                      <Modal.Title style={{color:'#274F77'}}>Inserisci email</Modal.Title>
-                    </Modal.Header>
-        
-                    <Modal.Body style={{width:'350px', padding:'7px'}}><input type='text' style={inputTextStyle}></input></Modal.Body>
-                      <Modal.Footer style={{width:'350px'}}>
-                        <Button className='buttonHover button' variant="secondary" onClick={handleCloseEmail}>Annulla</Button>
-                        <Button className='buttonHover button' variant="primary" onClick={handleCloseEmail}>Inserisci</Button>
-                    </Modal.Footer>
-                </Modal>              
-                              
+          <Row>
+            <Col md={12}>
+              <Table hover>
+                <thead>
+                  <tr>
+                    {header.map((prop, key) => {
+                      return <th key={key}>{prop}</th>;
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {professors &&
+                    professors.map(element => {
+                      return (
+                        <tr
+                          key={element.serialNumber}
+
+                        >
+                          <td>{element.email}</td>
+                          <td>{element.name}</td>
+                          <td>{element.surname}</td>
+                          <td onClick={() => { handleShow(element) }}><i className="pe-7s-trash trashIcon" style={styleIconTrash}></i></td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+
+          <Modal style={{ borderRadius: '6px', overflow: 'hidden', marginTop: '15%', left: '45%', position: 'absolute', height: '210px', width: '350px' }} show={this.state.show} onHide={handleClose} animation={false}>
+            <Modal.Header style={{ width: '350px' }} closeButton>
+              <Modal.Title style={{ color: '#274F77' }}>Elimina professore</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body style={{ width: '350px', padding: '7px' }}>Confermare l'eliminazione?</Modal.Body>
+            <Modal.Footer style={{ width: '350px' }}>
+              <Button className='buttonHover button' variant="secondary" onClick={handleClose}>Annulla</Button>
+              <Button className='buttonHover button' variant="primary" onClick={deleteProfessor}>Elimina</Button>
+            </Modal.Footer>
+          </Modal>
+          {/* Modal to insert a email */}
+          <Modal style={{ borderRadius: '6px', overflow: 'hidden', marginTop: '15%', left: '45%', position: 'absolute', height: '210px', width: '350px' }} show={this.state.showInsertEmail} onHide={handleCloseEmail} animation={false}>
             
-            </Grid>
+            <Modal.Header style={{ width: '350px' }} closeButton>
+              <Modal.Title style={{ color: '#274F77' }}>Inserisci email</Modal.Title>
+            </Modal.Header>
 
-            </div>
-          );
-        }
+            <Modal.Body style={{ width: '350px', padding: '7px' }}>
+              <input id='emailText' type='text' style={inputTextStyle}></input>
+              <div id='errorEmail'></div>
+            </Modal.Body>
+            
+            <Modal.Footer style={{ width: '350px' }}>
 
-      }
+              <Button className='buttonHover button' variant="secondary" onClick={handleCloseEmail}>Annulla</Button>
+              <Button className='buttonHover button' variant="primary" onClick={()=>{setEmailVerified(document.getElementById('emailText').value)}}>Inserisci</Button>
+            </Modal.Footer>
+          </Modal>
+
+
+        </Grid>
+
+      </div>
+    );
+  }
+
+}
 export default ProfessorList;
--- src/components/Users/StudentList.jsx
+++ src/components/Users/StudentList.jsx
@@ -1,60 +1,60 @@
-import React,{Component} from 'react';
-import {Grid, Row, Table, Col, Modal} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Grid, Row, Table, Col, Modal } from 'react-bootstrap';
 import Button from '../CustomButton/CustomButton'
 import axios from 'axios'
 
-export class StudentList extends Component{
-    state={
-        header:[],
-        students:[],
-        selectedStudent: '',
-        show: false,
+export class StudentList extends Component {
+  state = {
+    header: [],
+    students: [],
+    selectedStudent: '',
+    show: false,
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    //Headers sends to the search request for authentication.
+    const headers = {
+      'Authorization': localStorage.getItem('token'),
     }
+    //Data sends to the search request.
+    let data = {
+      param: {
+        role: 'Student',
+      }
+    };
 
-    constructor(props){
-        super(props);
-    }
+    this.setState({
+      header: ['E-mail', 'Nome', 'Cognome', 'Matricola', 'Data di Nascita'],
+    });
 
-    componentDidMount(){
-        //Headers sends to the search request for authentication.
-        const headers = {
-          'Authorization': localStorage.getItem('token'),
-        }
-        //Data sends to the search request.
-        let data={
-          param:{
-            role: 'Student',
-          } 
-        };
-        
-        this.setState({
-            header:['E-mail','Nome','Cognome','Matricola','Data di Nascita'],
-        });
-
-        axios
-        .post('http://localhost:3001/api/users/search',data,
+    axios
+      .post('http://localhost:3001/api/users/search', data,
         {
-          headers:headers,
+          headers: headers,
         })
-        .then(blob => {
-          this.setState({
-            students: blob.data.list,
-          })
-        });
+      .then(blob => {
+        this.setState({
+          students: blob.data.list,
+        })
+      });
 
-    }
+  }
 
-    render(){
-      //Function to delete selected student.
-    
-      const deleteStudent = ()=>{
-        //Search the student to delete.
-       
-        const id = this.state.selectedStudent.email; 
-        let url ='http://localhost:3001/api/users/'+id;
-        axios
-        .get(url,{
-          headers:{
+  render() {
+    //Function to delete selected student.
+
+    const deleteStudent = () => {
+      //Search the student to delete.
+
+      const id = this.state.selectedStudent.email;
+      let url = 'http://localhost:3001/api/users/' + id;
+      axios
+        .get(url, {
+          headers: {
             'Authorization': localStorage.getItem('token'),
           },
         })
@@ -62,105 +62,106 @@ export class StudentList extends Component{
           this.setState({
             selectedStudent: blob.data.user,
           })
+
+          const headers = {
+            'Authorization': localStorage.getItem('token'),
+          }
+          //Delete the user selected.
+          axios.
+            delete('http://localhost:3001/api/users/' + this.state.selectedStudent.email, {
+              headers: headers,
+            })
+            .then(blob => {
+
+              this.setState({
+                students: students.filter((el) => el.email != this.state.selectedStudent.email),
+              })
+
+            });
+
         });
+      handleClose();
 
-        const headers={
-          'Authorization': localStorage.getItem('token'),
-        }
-        //Delete the user selected.
-        axios.
-        delete('http://localhost:3001/api/users/'+this.state.selectedStudent.email,{
-          headers: headers,
-        },)
-        .then(blob=>{
-          
-          this.setState({
-            students: students.filter((el)=>el.email!=this.state.selectedStudent.email),
-          })
+    }
 
-        });
-        handleClose();
-        
-      }
-      
-    
-      const handleClose = () => this.setState({
-        show:false,
-      });
 
-    
-      const handleShow = (student) => this.setState({
-        show:true,
-        selectedStudent: student,
-      });
+    const handleClose = () => this.setState({
+      show: false,
+    });
 
-      const{students} = this.state;
-           
-      const {
-            header,
-        }=this.state;
-      
-      const styleIconTrash={
-        fontSize:'20px',
-        color:'#274F77',
-      }
-        return (
-            <div className='content'>
-              <Grid fluid>
-                <div>   
-                  <input className='inputSearchBar' type='text' placeholder='Cerca...'/>
-                  <span><i className='pe-7s-search iconSearchBar'></i></span>
-                </div>
-                <Row>
-                  <Col md={12}>
-                        <Table hover>
-                          <thead>
-                            <tr>
-                              {header.map((prop, key) => {
-                                return <th key={key}>{prop}</th>;
-                              })}
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {students &&
-                              students.map(element => {
-                                return (
-                                  <tr
-                                    key={element.serialNumber}
-                               
-                                  >
-                                    <td>{element.email}</td>
-                                    <td>{element.name}</td>
-                                    <td>{element.surname}</td>                                    
-                                    <td>{element.registration_number}</td>
-                                    <td>{element.birth_date.split("T")[0]}</td>
-                                    <td onClick={()=>{handleShow(element)}} ><i className="pe-7s-trash trashIcon" style={styleIconTrash}></i></td>
-                                  </tr>
-                                );
-                                
-                              })}
-                          </tbody>
-                        </Table>
-                  </Col>
-                </Row>
-                 
-                  {/*This is the popup shown when you try to delete a student*/}
-                  <Modal style={{borderRadius:'6px',overflow:'hidden',marginTop:'15%',left:'45%',position:'absolute',height:'200px',width:'350px'}} show={this.state.show} onHide={handleClose} animation={false}>
-                    <Modal.Header style={{width:'350px'}} closeButton>
-                      <Modal.Title style={{color:'#274F77'}}>Elimina studente</Modal.Title>
-                    </Modal.Header>
-        
-                    <Modal.Body style={{width:'350px', padding:'7px'}}>Confermare l'eliminazione?</Modal.Body>
-                      <Modal.Footer style={{width:'350px'}}>
-                        <Button className='buttonHover button' variant="secondary" onClick={handleClose}>Annulla</Button>
-                            <Button className='buttonHover button' variant="primary" onClick={deleteStudent}>Elimina</Button>
-                    </Modal.Footer>
-                  </Modal>
-                  
-              </Grid>
-            </div>
-          );
-        }
 
-      }
+    const handleShow = (student) => this.setState({
+      show: true,
+      selectedStudent: student,
+    });
+
+    const { students } = this.state;
+
+    const {
+      header,
+    } = this.state;
+
+    const styleIconTrash = {
+      fontSize: '20px',
+      color: '#274F77',
+    }
+    return (
+      <div className='content'>
+        <Grid fluid>
+          <div>
+            <input className='inputSearchBar' type='text' placeholder='Cerca...' />
+            <span><i className='pe-7s-search iconSearchBar'></i></span>
+          </div>
+          <Row>
+            <Col md={12}>
+              <Table hover>
+                <thead>
+                  <tr>
+                    {header.map((prop, key) => {
+                      return <th key={key}>{prop}</th>;
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {students &&
+                    students.map(element => {
+                      return (
+                        <tr
+                          key={element.serialNumber}
+
+                        >
+                          <td>{element.email}</td>
+                          <td>{element.name}</td>
+                          <td>{element.surname}</td>
+                          <td>{element.registration_number}</td>
+                          <td>{element.birth_date.split("T")[0]}</td>
+                          <td onClick={() => { handleShow(element) }} ><i className="pe-7s-trash trashIcon" style={styleIconTrash}></i></td>
+                        </tr>
+                      );
+
+                    })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+
+          {/*This is the popup shown when you try to delete a student*/}
+          <Modal style={{ borderRadius: '6px', overflow: 'hidden', marginTop: '15%', left: '45%', position: 'absolute', height: '200px', width: '350px' }} show={this.state.show} onHide={handleClose} animation={false}>
+            <Modal.Header style={{ width: '350px' }} closeButton>
+              <Modal.Title style={{ color: '#274F77' }}>Elimina studente</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body style={{ width: '350px', padding: '7px' }}>Confermare l'eliminazione?</Modal.Body>
+            <Modal.Footer style={{ width: '350px' }}>
+              <Button className='buttonHover button' variant="secondary" onClick={handleClose}>Annulla</Button>
+              <Button className='buttonHover button' variant="primary" onClick={deleteStudent}>Elimina</Button>
+            </Modal.Footer>
+          </Modal>
+
+        </Grid>
+      </div>
+    );
+  }
+
+}
 export default StudentList;
