fbd8434ae29c0019abc81826bcbf27d20b864add
MarkoMark98 <52664407+MarkoMark98@users.noreply.github.com>
2020-01-10 16:54:49 +0100

    SignUp [RESPONSIVE]

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -7,6 +7,7 @@ import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap
 import classnames from 'classnames';
 import bg from '../assets/img/background-image2.jpg'
 import GuestNavbar from '../components/Navbars/GuestNavbar';
+import '../assets/css/signup.css'
 
 const SignUp = (props) => {
     const [activeTab, setActiveTab] = useState('1');
@@ -23,6 +24,7 @@ const SignUp = (props) => {
             document.getElementById('1').style.borderWidth = '1px'
             document.getElementById('1').style.borderColor = '#81818175'
             document.getElementById('1').style.borderRadius = '3px'
+            document.getElementById('1').firstChild.style.color = '#274F77'
 
             document.getElementById('2').style.borderTopStyle = 'none'
             document.getElementById('2').style.borderLeftStyle = 'none'
@@ -31,6 +33,7 @@ const SignUp = (props) => {
             document.getElementById('2').style.borderWidth = '0px'
             document.getElementById('2').style.borderColor = 'white'
             document.getElementById('2').style.borderRadius = '3px'
+            document.getElementById('2').firstChild.style.color = 'black'
         } else {
             document.getElementById('2').style.borderTopStyle = 'solid'
             document.getElementById('2').style.borderLeftStyle = 'solid'
@@ -39,6 +42,7 @@ const SignUp = (props) => {
             document.getElementById('2').style.borderWidth = '1px'
             document.getElementById('2').style.borderColor = '#81818175'
             document.getElementById('2').style.borderRadius = '3px'
+            document.getElementById('2').firstChild.style.color = '#274F77'
 
             document.getElementById('1').style.borderTopStyle = 'none'
             document.getElementById('1').style.borderLeftStyle = 'none'
@@ -47,74 +51,68 @@ const SignUp = (props) => {
             document.getElementById('1').style.borderWidth = '0px'
             document.getElementById('1').style.borderColor = 'white'
             document.getElementById('1').style.borderRadius = '3px'
+            document.getElementById('1').firstChild.style.color = 'black'
         }
     }
 
-    const setBackground = ()=>{
-        document.body.style.backgroundImage = 'url('+bg+')'
+    const setBackground = () => {
+        document.body.style.backgroundImage = 'url(' + bg + ')'
         document.body.style.backgroundRepeat = 'no-repeat'
         document.body.style.backgroundSize = 'cover'
         document.body.style.backgroundPosition = 'center'
     }
 
     return (
-        <div onLoad={() => { toggle('1'); setBackground();}}>
+        <div onLoad={() => { toggle('1'); setBackground(); }}>
             <GuestNavbar />
-            <div style={{
-                position: 'absolute',
-                width: '98vw',
-                margin:'auto'
-            }}>
-                <Row style={{
-                    width: '100%',
-                    overflowY: 'hidden',
-                    marginLeft:'1px'
-                }}>
-                    <Col md="2" lg='2' sm='1'></Col>
-                    <Col md="8" lg='8' sm='10'>
-                        <Card
-                            title='Registrati'
-                            content={
-                                <div>
-                                    <Nav tabs>
-                                        <NavItem id='1'>
-                                            <NavLink
-                                                className={classnames({ active: activeTab === '1' }), 'tabLink'}
-                                                onClick={() => { toggle('1'); }}
-                                            >
-                                                Studente
+            <div className='container-div'>
+                    <Row className='container-row'>
+                        <Col md="2" lg='2' sm='1'></Col>
+                        <Col md="8" lg='8' sm='10'>
+                            <Card
+                                className='card'
+                                title={<b style={{ color: '#274F77' }}>Registrati</b>}
+                                content={
+                                    <div>
+                                        <Nav tabs>
+                                            <NavItem id='1'>
+                                                <NavLink
+                                                    className={classnames({ active: activeTab === '1' })}
+                                                    onClick={() => { toggle('1'); }}
+                                                >
+                                                    Studente
                                                     </NavLink>
-                                        </NavItem>
-                                        <NavItem id='2'>
-                                            <NavLink
-                                                className={classnames({ active: activeTab === '2' }), 'tabLink'}
-                                                onClick={() => { toggle('2'); }}
-                                            >
-                                                Docente
+                                            </NavItem>
+                                            <NavItem id='2'>
+                                                <NavLink
+                                                    className={classnames({ active: activeTab === '2' })}
+                                                    onClick={() => { toggle('2'); }}
+                                                >
+                                                    Docente
                                                     </NavLink>
-                                        </NavItem>
-                                    </Nav>
-                                    <TabContent activeTab={activeTab}>
-                                        <TabPane tabId="1">
-                                            <Row>
-                                                <Col sm="12">
-                                                    <SignUpStudent />
-                                                </Col>
-                                            </Row>
-                                        </TabPane>
-                                        <TabPane tabId="2">
-                                            <Row>
-                                                <Col sm="12">
-                                                    <SignUpProfessor />
-                                                </Col>
-                                            </Row>
-                                        </TabPane>
-                                    </TabContent>
-                                </div>
-                            }
-                        />
-                    </Col>
-                </Row>
+                                            </NavItem>
+                                        </Nav>
+                                        <TabContent activeTab={activeTab}>
+                                            <TabPane tabId="1">
+                                                <Row>
+                                                    <Col sm="12">
+                                                        <SignUpStudent />
+                                                    </Col>
+                                                </Row>
+                                            </TabPane>
+                                            <TabPane tabId="2">
+                                                <Row>
+                                                    <Col sm="12">
+                                                        <SignUpProfessor />
+                                                    </Col>
+                                                </Row>
+                                            </TabPane>
+                                        </TabContent>
+                                    </div>
+                                }
+                            />
+                        </Col>
+                    </Row>
             </div>
         </div>
     )
