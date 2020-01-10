625d9ba94a0e62b8cc1aa6e37b4b86823b37b8c3
Pepp1 <giuseppe.facenda@live.it>
2020-01-10 10:29:03 +0100

    Gestiti errori nella pagina di login

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,62 +1,60 @@
 import axios from "axios";
 
-export const dictionaryPath ={
-    student : 'Student',
-    professor: 'Professor',
-    ddi: 'DDI',
-    admin: 'Teaching Office',
-}
+export const dictionaryPath = {
+  student: "Student",
+  professor: "Professor",
+  ddi: "DDI",
+  admin: "Teaching Office"
+};
 
-export const isLogin = (path) => {
-    let token = localStorage.getItem('token');
-    let user = JSON.parse(localStorage.getItem('user'));
-    path = path.Path.split("/")[1];
-    if (token != null && dictionaryPath[''+path]===user.role){
-        return true;
-    }
-    else if(token != null && dictionaryPath[''+path]!=user.role){
-        return 'Not Authorized';
-    }
-    else
-        return false;
-}
+export const isLogin = path => {
+  let token = localStorage.getItem("token");
+  let user = JSON.parse(localStorage.getItem("user"));
+  path = path.Path.split("/")[1];
+  if (token != null && dictionaryPath["" + path] === user.role) {
+    return true;
+  } else if (token != null && dictionaryPath["" + path] != user.role) {
+    return "Not Authorized";
+  } else return false;
+};
 
-export const login = (userEmail, userPassword) =>{
-    axios
-    .post('http://localhost:3001/api/auth/login', {
+export const login = (userEmail, userPassword) => {
+  return axios
+    .post("http://localhost:3001/api/auth/login", {
       user: {
         email: userEmail,
-        password: userPassword,
+        password: userPassword
       }
     })
-    .then(blob =>{
-            localStorage.setItem('status', blob.data.status);
-            localStorage.setItem('token',blob.data.token);
-            localStorage.setItem('user', JSON.stringify(blob.data.user));
-            let user = JSON.parse(localStorage.getItem('user'));
-            switch(user.role){
-     
-                case 'DDI': window.location.replace("http://localhost:3000/ddi/notices");
-                    break;
-                case 'Professor': window.location.replace("http://localhost:3000/professor/notices");
-                    break;
-                case 'Student': window.location.replace("http://localhost:3000/student/notices");    
-                    break;
-                case 'Teaching Office': window.location.replace("http://localhost:3000/admin/notices");
-                    break;
-            }
+    .then(blob => {
+      localStorage.setItem("status", blob.data.status);
+      localStorage.setItem("token", blob.data.token);
+      localStorage.setItem("user", JSON.stringify(blob.data.user));
+      let user = JSON.parse(localStorage.getItem("user"));
+      switch (user.role) {
+        case "DDI":
+          window.location.replace("http://localhost:3000/ddi/notices");
+          break;
+        case "Professor":
+          window.location.replace("http://localhost:3000/professor/notices");
+          break;
+        case "Student":
+          window.location.replace("http://localhost:3000/student/notices");
+          break;
+        case "Teaching Office":
+          window.location.replace("http://localhost:3000/admin/notices");
+          break;
+      }
+    })
+    .catch(error => {
+      throw error.response.data.error;
+    });
+};
 
-        });
-        
-        
-}
+export const logout = () => {
+  localStorage.removeItem("status");
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
 
-
-export const logout = () =>{
-    localStorage.removeItem('status');
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-
-    window.location.replace("http://localhost:3000/home");
-
-}
+  window.location.replace("http://localhost:3000/home");
+};
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -4,18 +4,21 @@ import {
   FormControl,
   ControlLabel,
   Row,
-  Col
+  Col,
+  Alert
 } from "react-bootstrap";
 import "../assets/css/page-login.css";
 import Card from "../components/Card/Card";
 import Button from "components/CustomButton/CustomButton";
-import bg from '../assets/img/background-image2.jpg'
+import bg from "../assets/img/background-image2.jpg";
 import GuestNavbar from "../components/Navbars/GuestNavbar";
 import { login } from "../utils/auth";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [loginError, setLoginError] = useState("");
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -26,31 +29,46 @@ export default function Login(props) {
   }
 
   const setBackground = () => {
-    document.body.style.backgroundImage = 'url(' + bg + ')'
-    document.body.style.backgroundRepeat = 'no-repeat'
-    document.body.style.backgroundSize = 'cover'
-    document.body.style.backgroundPosition = 'center'
-  }
+    document.body.style.backgroundImage = "url(" + bg + ")";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  };
   return (
-    <div onLoad={() => { setBackground() }}>
+    <div
+      onLoad={() => {
+        setBackground();
+      }}
+    >
       <GuestNavbar />
       <div
         style={{
-          position: 'absolute',
-          width: '98vw'
-        }} />
-      <Row className="Login_Card" style={{
-        width: '100%',
-        overflowY: 'hidden',
-        maxHeight: '550px',
-        padding: '1px',
-      }}>
-        <Col md="4" lg='4' sm='2' xs='2'></Col>
-        <Col md="4" lg='4' sm='8' xs='8' style={{marginTop:'6vw'}}>
+          position: "absolute",
+          width: "98vw"
+        }}
+      />
+      <Row
+        className="Login_Card"
+        style={{
+          width: "100%",
+          overflowY: "hidden",
+          maxHeight: "550px",
+          padding: "1px"
+        }}
+      >
+        <Col md="4" lg="4" sm="2" xs="2"></Col>
+        <Col md="4" lg="4" sm="8" xs="8" style={{ marginTop: "6vw" }}>
           <Card
             content={
               <div className="Login">
                 Login
+                {showError === true ? (
+                  <Alert bsStyle="danger">
+                    <p>{loginError}</p>
+                  </Alert>
+                ) : (
+                  <div></div>
+                )}
                 <form onSubmit={handleSubmit}>
                   <FormGroup controlId="email" bsSize="large">
                     <ControlLabel>Email</ControlLabel>
@@ -74,8 +92,16 @@ export default function Login(props) {
                     bsSize="large"
                     disabled={!validateForm()}
                     type="submit"
-                    onClick={()=>{login(email,password)}}
-                    >
+                    onClick={() => {
+                      login(email, password)
+                        .then(blob => {})
+                        .catch(error => {
+                          setLoginError(error);
+
+                          setShowError(true);
+                        });
+                    }}
+                  >
                     Invio
                   </Button>
                 </form>
